cff1e710ee55c7cc9a6b29f536aab8dabf24c1e6
charrus <charlie@rusbridger.com>
2024-11-08 11:34:33 +0000

    Sync

--- src/mythcommflagwrapper/const.py
+++ src/mythcommflagwrapper/const.py
@@ -1,9 +1,4 @@
-"""constants.
-
-Constants missing from MythTV python bindings
-"""
-
-# From: https://github.com/MythTV/mythtv/blob/master/mythtv/libs/libmythbase/programtypes.h#L128
+# From: https://github.com/MythTV/mythtv/blob/master/mythtv/libs/libmythbase/programtypes.h#L128  # noqa: D100 E501
 COMM_DETECT_COMMFREE = -2
 COMM_DETECT_UNINIT = -1
 COMM_DETECT_OFF = 0x00000000
